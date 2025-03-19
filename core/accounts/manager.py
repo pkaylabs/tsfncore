e997875f9a20d52d81ab7011056af1a92d8755db
princek <princesamuelpks@gmail>
2025-03-19 17:13:08 +0000

    initial setup

---
+++ core/accounts/manager.py
@@ -0,0 +1,51 @@
+from django.contrib.auth.models import BaseUserManager
+
+
+class AccountManager(BaseUserManager):
+    '''Manages User account creation'''
+
+    def create_user(self, email, password=None, name=None, phone=None, address=None, **kwargs):
+        '''Create a regular user'''
+        if not email:
+            raise ValueError('The Email field must be set')
+        if not phone:
+            raise ValueError('The Phone field must be set')
+        if not password:
+            raise ValueError('The Password field must be set')
+
+        email = self.normalize_email(email)
+        user = self.model(
+            email=email,
+            name=name,
+            phone=phone,
+            address=address,
+            **kwargs
+        )
+        user.set_password(password)
+        user.is_staff = False
+        user.is_superuser = False
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, name=None, phone=None, address=None, **kwargs):
+        '''Create a superuser'''
+        if not email:
+            raise ValueError('The Email field must be set')
+        if not phone:
+            raise ValueError('The Phone field must be set')
+        if not password:
+            raise ValueError('The Password field must be set')
+
+        user = self.create_user(
+            phone=phone,
+            name=name,
+            email=email,
+            password=password,
+            address=address,
+            **kwargs
+        )
+        user.is_staff = True
+        # TODO: Think through the implications again.
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
---
+++ core/accounts/models.py
@@ -0,0 +1,65 @@
+'''
+This module contains the models for the accounts.
+It includes the User, and OTP models.
+These models are used to store information about the users 
+and their otp information.
+
+'''
+
+from datetime import timedelta, timezone
+
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.db import models
+
+from dxpcore.utils.services import send_mail
+
+from .manager import AccountManager
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    '''Custom User model for the application'''
+    email = models.EmailField(max_length=50, unique=True)
+    phone = models.CharField(max_length=12, unique=True)
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=500, blank=True, null=True)
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+
+    deleted = models.BooleanField(default=False)  # Soft delete
+    
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    created_from_app = models.BooleanField(default=False)
+    phone_verified = models.BooleanField(default=False)
+    email_verified = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = AccountManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['phone', 'name']
+
+    def __str__(self):
+        return self.name
+
+class OTP(models.Model):
+    '''One Time Password model'''
+    email = models.CharField(max_length=100)
+    otp = models.CharField(max_length=6)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def is_expired(self) -> bool:
+        '''Returns True if the OTP is expired'''
+        return (self.created_at + timedelta(minutes=30)) < timezone.now()
+    
+    def send_otp_to_user(self) -> None:
+        '''Send the OTP to the user'''
+        msg = f'Welcome to the Destination Experience App.\nYour OTP is {self.otp}\n\nRegards,\nDXP Team'
+        send_mail([self.email], 'OTP', msg)
+
+    def __str__(self):
+        return self.email + ' - ' + self.otp
